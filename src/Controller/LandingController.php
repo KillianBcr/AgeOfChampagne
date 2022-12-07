d72ab6bcc9fd93ff78e252d21510384867e8faff
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2022-12-07 13:29:44 +0100

    Ajout/coorection de la page Landing(acceuil)

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class LandingController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/landing', name: 'app_landing')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('Landing/Landing.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
