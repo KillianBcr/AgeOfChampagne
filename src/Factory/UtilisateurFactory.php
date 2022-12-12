530ebfb054769c88a0b788450c5f19068c56b5c8
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-12-12 13:53:47 +0100

    Merge branch 'fixtures' into 'main'
    Ajout des fixtures de l'entité User
    
    See merge request sami0002/sae3-01!12
    
    (cherry picked from commit 4ab55ded6f2347e28541db9eef0467c3f14ff9f9)
    
    d8d24416 création d'une forge de données
    3359d13f modifivation du fichier src/Factory/UtilisateurFactory.php
    c9853b16 modification de la methode getDefaults()
    04164ec1 ajout du constructeur de l'Entité Utilisateur
    1ae610ad modification de la methode load() de src/DataFixtures/UserFixtures

---
+++ src/Factory/UtilisateurFactory.php
@@ -0,0 +1,73 @@
+// src/Factory/PostFactory.php
+
+namespace App\Factory;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Post>
+ *
+ * @method static Post|Proxy createOne(array $attributes = [])
+ * @method static Post[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Post[]&Proxy[] createSequence(array|callable $sequence)
+ * @method static Post|Proxy find(object|array|mixed $criteria)
+ * @method static Post|Proxy findOrCreate(array $attributes)
+ * @method static Post|Proxy first(string $sortedField = 'id')
+ * @method static Post|Proxy last(string $sortedField = 'id')
+ * @method static Post|Proxy random(array $attributes = [])
+ * @method static Post|Proxy randomOrCreate(array $attributes = []))
+ * @method static Post[]|Proxy[] all()
+ * @method static Post[]|Proxy[] findBy(array $attributes)
+ * @method static Post[]|Proxy[] randomSet(int $number, array $attributes = []))
+ * @method static Post[]|Proxy[] randomRange(int $min, int $max, array $attributes = []))
+ * @method static PostRepository|RepositoryProxy repository()
+ * @method Post|Proxy create(array|callable $attributes = [])
+ */
+final class PostFactory extends ModelFactory
+{
+    /**
+     * @see https://github.com/zenstruck/foundry#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://github.com/zenstruck/foundry#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+    return [
+        'firstname' => self::faker()->sentence(),
+        'lastname' => self::faker()->sentence(),
+	'email' => self::numerify('####@example.com')->unique()->sentence(),
+	'password' => self::faker()->sentence(),
+	'telUtil' => self::numerify('##########')->unique()->sentence(),
+	'dateNais' => self::faker()->sentence(),
+    ];'
+    }
+
+    /**
+     * @see https://github.com/zenstruck/foundry#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Post $post) {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Post::class;
+    }
+}
