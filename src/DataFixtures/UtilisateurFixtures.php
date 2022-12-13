154450157cc9663394fc2397bab1febeb9d80884
bosc0005 <killian.boscher@etudiant.univ-reims.fr>
2022-12-13 09:07:44 +0100

    création d'un utilisateur admin

---
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\UtilisateurFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UtilisateurFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): \App\Entity\Utilisateur|\Zenstruck\Foundry\Proxy
+    {
+        $admin = UtilisateurFactory::createOne([
+            'nom' => 'Boscher',
+            'prenom' => 'Killian',
+            'email' => 'root@example.com',
+            'roles' => 'ROLE_ADMIN',
+        ]);
+        return $admin;
+    }
+}
