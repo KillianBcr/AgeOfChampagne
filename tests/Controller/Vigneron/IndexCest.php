6d580a25d728a99c888eeac6081d9f234062a8a9
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2022-12-14 16:43:28 +0100

    ajout des fichiers nessesaire aux tests

---
+++ tests/Controller/Vigneron/IndexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Vigneron;
+
+use App\Tests\ControllerTester;
+
+class IndexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
