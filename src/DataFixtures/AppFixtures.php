b7e84cc26445fa406a47c43c31d5075d8228731d
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-12-19 22:04:39 +0100

    Modification des fixtures pour ajouter un admin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $admin = new Utilisateur();
+        $admin->setNom('SAMINA');
+        $admin->setPrenom('Angelo');
+        $admin->setEmail('root@example.com');
+        $admin->setTelephone('0612345678');
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $password = $this->userPasswordHasher->hashPassword($admin,"test");
+        $admin->setPassword($password);
+        $manager->persist($admin);
         $manager->flush();
     }
 }
