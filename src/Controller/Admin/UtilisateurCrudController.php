8a570c5373793c780563481fb11de4e891aaf815
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-12-27 13:46:35 +0100

    Ajout de caractéristiques au champ mot de passe

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
@@ -12,14 +17,26 @@ class UtilisateurCrudController extends AbstractCrudController
         return Utilisateur::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            TextField::new('telephone'),
+            ArrayField::new('roles'),
+
+            // caractéristiques du champs de mot de passe
+            TextField::new('password')
+            // Uniquement visible dans le formulaire
+                ->hideOnIndex()
+            // Non obligatoire
+                ->setRequired(false)
+            // Valeur vide
+                ->setEmptyData('')
+            // Type de champs
+                ->setFormType(PasswordType::class),
+            EmailField::new('email')->hideOnIndex(),
         ];
     }
-    */
 }
