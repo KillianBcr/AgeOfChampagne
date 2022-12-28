ec08f66868a5dfcae8c253dc1b9d20d1aee4c56b
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-12-28 14:34:45 +0100

    Création des migrations

---
+++ migrations/Version20221228132841.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221228132841 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE utilisateur ADD cp VARCHAR(5) NOT NULL, ADD ville VARCHAR(20) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE utilisateur DROP cp, DROP ville');
+    }
+}
