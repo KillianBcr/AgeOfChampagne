0e65ef6d3446430c0ccd041db5690de43c5a1ccd
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2022-12-30 17:09:42 +0100

    ajout de la méthode new() permettant de creer une fiche

--- src/Controller/FicheController.php
+++ src/Controller/FicheController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\FichePartenaire;
+use App\Form\FichePartenaireType;
 use App\Repository\FichePartenaireRepository;
-use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,10 +18,36 @@ class FicheController extends AbstractController
     {
         $fiches =
             $repository->findAll()
-       ;
+        ;
 
         return $this->render('pages/fiche/index.html.twig', [
             'fiches' => $fiches,
+        ]);
+    }
+
+    #[Route('/partenaire/fiche/creation', name: 'app_fiche_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $fiche = new FichePartenaire();
+        $form = $this->createForm(FichePartenaireType::class, $fiche);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $fiche = $form->getData();
+
+            $manager->persist($fiche);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre fiche a été créé avec succès !'
+            );
+
+            return $this->redirectToRoute('app_fiche');
+        }
+
+        return $this->render('pages/fiche/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
