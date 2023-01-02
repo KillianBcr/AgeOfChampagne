426b3c259de0ddf180322a6b39021d39b96490a6
Angelo Samina <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-01-02 11:03:25 +0100

    Restrcition de l'acces au utilisateurs possedant le role (ROLE_PARTENAIRE)

--- src/Controller/FicheController.php
+++ src/Controller/FicheController.php
@@ -6,6 +6,8 @@ use App\Entity\FichePartenaire;
 use App\Form\FichePartenaireType;
 use App\Repository\FichePartenaireRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FicheController extends AbstractController
 {
+    #[IsGranted('ROLE_PARTENAIRE')]
     #[Route('/partenaire/fiche', name: 'app_fiche', methods: ['GET'])]
     public function index(FichePartenaireRepository $repository, Request $request): Response
     {
@@ -25,6 +28,7 @@ class FicheController extends AbstractController
         ]);
     }
 
+    #[IsGranted('ROLE_PARTENAIRE')]
     #[Route('/partenaire/fiche/creation', name: 'app_fiche_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $manager): Response
     {
@@ -52,6 +56,7 @@ class FicheController extends AbstractController
         ]);
     }
     // Edition de la fiche partenaire
+    #[Security("is_granted('ROLE_PARTENAIRE') and utilisateur === app.getUser()")]
     #[Route('/partenaire/fiche/edition/{id}', 'app_fiche_edit', methods: ['GET', 'POST'])]
     public function edit(FichePartenaire $fiche, Request $request, EntityManagerInterface $manager): Response
     {
@@ -77,6 +82,7 @@ class FicheController extends AbstractController
         ]);
     }
     //Suppression
+    #[Security("is_granted('ROLE_PARTENAIRE') and utilisateur === app.getUser()")]
     #[Route('/partenaire/suppression/{id}', 'app_fiche_delete', methods: ['GET'])]
     public function delete(EntityManagerInterface $manager, FichePartenaire $fiche): Response
     {
