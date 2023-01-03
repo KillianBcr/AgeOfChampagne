cdae3c395cc9dde2580fefbc0b374c962223817f
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2023-01-03 17:34:37 +0100

    Création de CarteCrudController.php

---
+++ src/Controller/Admin/CarteCrudController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Carte;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+class CarteCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Carte::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+}
