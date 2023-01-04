0445a56c4f0682fb483218230773befbc3717af5
Alexis JACQUIN <alexis.jacquin@etudiant.univ-reims.fr>
2023-01-04 14:16:08 +0100

    Ajout du champs d'image au panel admin des cartes

--- src/Controller/Admin/CarteCrudController.php
+++ src/Controller/Admin/CarteCrudController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Carte;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
 class CarteCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -19,7 +21,7 @@ class CarteCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('qr_code'),
-            TextField::new('image_carte'),
+            ImageField::new('image_carte')->setUploadDir(''),
             TextField::new('name'),
             TextEditorField::new('description'),
         ];
