2dfc498cd91d65d920463204948a7968e3bba758
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-01-05 14:35:19 +0100

    modification du controller Carte

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Carte;
+use App\Repository\CarteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarteController extends AbstractController
 {
+
     #[Route('/carte', name: 'app_carte')]
-    public function index(): Response
+    public function index(CarteRepository $repository): Response
     {
-        return $this->render('carte/index.html.twig', [
-            'controller_name' => 'CarteController',
+        $cartes = $repository->findAllAlphabeticOrder();
+
+        return $this->render('pages/carte/index.html.twig', [
+            'cartes' => $cartes,
         ]);
+    }
+
+
+    #[Route('/carte/{id}', name: 'app_carte_show',
+        requirements: [
+            'id' => "\d+",
+        ]
+    )]
+    public function show(Carte $carte): Response
+    {
+
+        return $this->render('pages/carte/show.html.twig',
+            ['carte' => $carte]);
     }
 }
