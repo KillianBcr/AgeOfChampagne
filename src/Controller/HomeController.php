05540fe9b9d8d1aba9dadafb8198e7fa878073e6
Killian <killianboscher@gmail.com>
2023-01-09 11:46:16 +0100

    Création du controller + modification du formulaire + ajout de celui-ci sur Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,45 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    #[Route('/', name: 'comment_form', methods: ['GET', 'POST'])]
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('pages/home.html.twig', [
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Commentaire ajoutée !'
+            );
+            return $this->redirectToRoute('/');
+
+        }
+
+        return $this->renderForm('pages/home.html.twig', [
             'controller_name' => 'HomeController',
+            'comment_form' => $form,
         ]);
     }
 }
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Comment;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class CommentType extends AbstractType
 {
@@ -19,6 +23,14 @@ class CommentType extends AbstractType
                 'label' => 'Commentaire',
                 'label_attr' => [
                     'class' => 'textform',
+                ],
+                'constraints' => [
+                    new Length(['min' => 5, 'max' => 255]),
+                ],
+            ])
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => [
+                    'class' => 'send',
                 ],
             ])
         ;
