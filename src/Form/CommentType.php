590c4dbcfb4dab41b0b37c517b491ed181647fa0
Killian <killianboscher@gmail.com>
2023-01-09 11:18:55 +0100

    création du formulaire d'ajout de commentaire

---
+++ src/Form/CommentType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextType::class, [
+                'attr' => [
+                    'class' => 'formEntry',
+                ],
+                'label' => 'Commentaire',
+                'label_attr' => [
+                    'class' => 'textform',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
