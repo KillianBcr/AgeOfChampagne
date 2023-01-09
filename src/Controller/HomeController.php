aac57821440ddd6eb2e5a2c883d0e01d5ef66194
Killian <killianboscher@gmail.com>
2023-01-09 17:20:52 +0100

    modification importantes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,49 +2,19 @@
 
 namespace App\Controller;
 
-use App\Entity\Comment;
-use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\CommentType;
-use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Component\HttpFoundation\Request;
-
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("home", name="app_home")
+     * @Route("/", name="app_home")
      */
-    #[IsGranted('ROLE_USER')]
-    #[Route(name: 'comment_form', methods: ['GET', 'POST'])]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(): Response
     {
-        $comment = new Comment();
-        $form = $this->createForm(CommentType::class, $comment);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $comment = $form->getData();
-            if (strlen($comment->getSender()) == 0 ) {
-                $comment->setSender("Anonyme");
-            }
-            $manager->persist($comment);
-            $manager->flush();
-
-            $this->addFlash(
-                'success',
-                'Commentaire ajoutée !'
-            );
-
-        }
-
-        return $this->renderForm('pages/home.html.twig', [
+        return $this->render('pages/home.html.twig', [
             'controller_name' => 'HomeController',
-            'comment' => $form,
         ]);
     }
 }
