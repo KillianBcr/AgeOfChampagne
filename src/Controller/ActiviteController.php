d213537306ac9c70713ec22934fcd650f09ae0b7
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-01-10 16:31:30 +0100

    correction

--- src/Controller/ActiviteController.php
+++ src/Controller/ActiviteController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use App\Entity\Activite;
 use App\Repository\ActiviteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use http\Client\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
