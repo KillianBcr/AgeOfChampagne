ab40027b2f0aea977aa294c14199e7aeba74dd02
dell0014 <mael.delloque@etudiant.univ-reims.fr>
2023-01-11 11:48:28 +0100

    modification des fixtures de Crus et Cepages

--- src/DataFixtures/CrusFixtures.php
+++ src/DataFixtures/CrusFixtures.php
@@ -3,12 +3,41 @@
 namespace App\DataFixtures;
 
 use App\Entity\Crus;
+use App\Factory\CrusFactory;
+use App\Repository\CepageRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class CrusFixtures extends Fixture
 {
+    public CepageRepository $repository;
+    public function __construct(CepageRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     public function load(ObjectManager $manager): void
     {
+        $chardonnay= file_get_contents(__DIR__ . '/data/Crus_Chardonnay.json',true);
+        $crusChardonnay = json_decode($chardonnay,true);
+
+        $cepageRepository = $this->repository;
+
+        foreach($crusChardonnay as $elmt)
+        {
+
+            CrusFactory::createOne([
+                'nom' => $elmt->nom,
+                'description' => $elmt->description,
+                'cepage' => $cepageRepository->find($elmt->cepage)
+            ]);
+        }
     }
+
+    public function getDependencies()
+    {
+        return [
+            CepageFixtures::class,
+        ];
+    }
+
 }
