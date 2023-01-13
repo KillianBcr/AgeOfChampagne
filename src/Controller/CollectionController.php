6e3a7ecdc74f69494b700746b7eb4549ec4e3934
Killian <killianboscher@gmail.com>
2023-01-13 14:56:44 +0100

    Affichage de la collection propre à chaque utilisateur

--- src/Controller/CollectionController.php
+++ src/Controller/CollectionController.php
@@ -25,13 +25,16 @@ class CollectionController extends AbstractController
     #[Route('/collection', name: 'app_collection')]
     public function showCollection(CollectionRepository $repository)
     {
+        $utilisateur = $this->getUser();
+        $idUtilisateur = $utilisateur->getId();
         $collection = $repository->findAll();
         $em = $this->getDoctrine()->getManager();
         $query = $em->createQuery(
-            'SELECT c.name, c.description, c.imageName, c.id FROM App\Entity\Carte c
-             WHERE c.id IN (SELECT coll.cardID FROM App\Entity\Collection coll WHERE coll.userID = USERID)');
+            'SELECT c.name, c.description, c.imageName FROM App\Entity\Carte c
+             WHERE c.id IN (SELECT coll.cardID FROM App\Entity\Collection coll WHERE coll.userID = :idUtilisateur)');
+            $query->setParameter('idUtilisateur',$idUtilisateur);
         $collection = $query->getResult();
-    
+
         return $this->render('pages/collection/index.html.twig', [
             'collection' => $collection
         ]);
